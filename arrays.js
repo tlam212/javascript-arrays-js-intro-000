1b1f76b631a81fffb90e322c82478e3a825fa4ef
Toni Lamont <tlam212@gmail.com>
2018-06-03 17:27:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray = ()
+function addElementToBeginningOfArray = (array, 'foo') {
+  const array = [1];
+}addElementToBeginningOfArray();
