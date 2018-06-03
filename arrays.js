b955074fc451dca3b603bdbe43571fa773d7fef9
Toni Lamont <tlam212@gmail.com>
2018-06-03 17:09:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element){
  var myArray = ['bed', 'bath', 'beyond'];
  myArray = [...myArray, "macys"]
-}
+}myArray;
